f1ecf80856102acb439599120712f25ff366d41b
Sean Duffy <seanduffy@live.com>
2014-02-08 16:21:51 +0000

    Basic question retreival

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,15 @@
 class QuestionsController < ApplicationController
 	def index
+    questions = Question.all
+    @rand_id = questions.sample.id
 	end
 
 	def create
 	end
 
 	def show
-	end
-
-	def update
+    @question = Question.find_by id: params[:id]
+    puts @question
 	end
 
 	def destroy
