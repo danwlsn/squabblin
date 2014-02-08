deac0733b6337ef0d3e8658c72c367195808d17f
Danny Wilson <dannywilson32@gmail.com>
2014-02-08 14:21:20 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
  ShinyHipster::Application.routes.draw do
+
+  root 'arguments#index'
+
+  resources :arguments
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
