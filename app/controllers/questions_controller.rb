25bd2cfc5e414e2800afc81d0912681971b2555a
Sean Duffy <seanduffy@live.com>
2014-02-08 17:58:23 +0000

    Code for creating questions and arguments

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,17 @@ class QuestionsController < ApplicationController
 	end
 
 	def create
+    question = params[:question][:title]
+    args_a_text = params[:question][:args_a]
+    args_b_text = params[:question][:args_b]
+    args_a = Argument.create(:text=>args_a_text)
+    args_b = Argument.create(:text=>args_b_text)
+    Question.create(:title=>question, :args_a=>args_a.id, :args_b=>args_b.id)
 	end
+
+  def new
+    @question = Question.new
+  end
 
 	def show
     @question = Question.find_by id: params[:id]
