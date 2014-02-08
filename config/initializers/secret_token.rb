8c5115477c2af9c4f862419ac6a1d697e045e6c8
Danny Wilson <dannywilson32@gmail.com>
2014-02-08 12:08:02 +0000

    rails bare bones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShinyHipster::Application.config.secret_key_base = 'fadfa93638f0ecf1cee614c5d8c05f6a4961a2be7a97a428a36710d304688c6c01ae327899126eb9d916d4de17f15673a4865c78ad017a5412bf116169b3ffdd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShinyHipster::Application.config.session_store :cookie_store, key: '_shiny-hipster_session'
