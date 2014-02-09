c22623336adb4307e248d7f04387fbb9dbcc9de2
Danny Wilson <dannywilson32@gmail.com>
2014-02-09 02:00:16 +0000

    fixed indentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
 
 	def create
-    arg_id = params[:comment][:args_id]
-    question = Question.find_by args_a: arg_id
-    if question == nil
-      question = Question.find_by args_b: arg_id
-    end
-    slug = question.slug
-    comment_text = params[:comment][:comment]
-    Comment.create(:comment=>comment_text, :args_id=> Integer(arg_id))
-    redirect_to question_path(slug)
+        arg_id = params[:comment][:args_id]
+        question = Question.find_by args_a: arg_id
+        if question == nil
+          question = Question.find_by args_b: arg_id
+        end
+        slug = question.slug
+        comment_text = params[:comment][:comment]
+        Comment.create(:comment=>comment_text, :args_id=> Integer(arg_id))
+        redirect_to question_path(slug)
 	end
 
 	def destroy
