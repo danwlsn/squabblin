a4fc7c0260114c8f5335e58ea4ab0dc815ccb5a5
Sean Duffy <seanduffy@live.com>
2014-02-09 00:58:40 +0000

    Add comments to JSON in API

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -21,9 +21,15 @@ class Api::QuestionsController < ApplicationController
 		question = questions.sample
     args_a = Argument.find_by id: question.args_a
     args_b = Argument.find_by id: question.args_b
+    comments_a = Comment.where(args_id: args_a.id).all
+    comments_b = Comment.where(args_id: args_b.id).all
+    args_a = args_a.as_json
+    args_b = args_b.as_json
+    args_a[:comments] = comments_a.as_json
+    args_b[:comments] = comments_b.as_json
 
     full_json = question.as_json
-    full_json[:arguments] = args_a.as_json, args_b.as_json
+    full_json[:arguments] = args_a, args_b
 
 		respond_with :questions => full_json
 	end
