cb34ee286cb33ff42679b96e0ffecdc2d91aec31
Danny Wilson <dannywilson32@gmail.com>
2014-02-09 00:01:42 +0000

    fixed redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,15 @@ class CommentsController < ApplicationController
 
 	def create
     arg_id = params[:comment][:args_id]
-    slug = Question.find_by args_a: arg_id
-    if slug == nil
-      slug = Question.find_by args_b: arg_id
+    question = Question.find_by args_a: arg_id
+    if question == nil
+      question = Question.find_by args_b: arg_id
     end
+    slug = question.slug
     comment_text = params[:comment][:comment]
     Comment.create(:comment=>comment_text, :args_id=> Integer(arg_id))
-    redirect_to "/questions/#{slug}", :status => :moved_permanently
+    # redirect_to "/questions/#{slug}", :status => :moved_permanently
+    redirect_to question_path(slug)
 	end
 
 	def destroy
