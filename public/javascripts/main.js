d03bc162af90d6717751abc59065e0d645025e34
Sean Duffy <seanduffy@live.com>
2014-02-09 12:59:41 +0000

    Remove old JS

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,9 @@
- // setters and getters
-
- var argumentone = function(one) {argumentone = one;}
- var argumenttwo = function(two) {argumenttwo = two;}
- var argumentthee = function(three) {argumentthree = three;}
-
-
 // Using multiple unit types within one animation.
  <!-- click funtion <button id="go">&raquo; Run</button> $( "#go" ).click(function() {} -->
-
-
- // hover effects
-
-
-
 
 $( document ).ready(function() {
 
   $(".argument").slideDown(800)
-
-  $(".argument").slideDown(1500)
 
   $(".bg-primary").hover(function(){
     $(this).css("opacity","1")
@@ -26,27 +11,11 @@ $( document ).ready(function() {
     $(this).css("opacity","0.7");
   });
 
-  //animationappear();
 });
 
 // this function will reverse the animation
 
 var animationReverse = function() {
-  // Handler for .ready() called.
-   /* $( "#firstblock" ).animate({
-
-    width: "0%",
-    opacity: 0,
-    marginLeft: "1.0in",
-    fontSize: "3em",
-    borderWidth: "1px"
-
-  }, 2500 );
-
-    argumentone()
-
-})*/
-
 
 $( "#secondblock" ).animate({
   width: "0%",
