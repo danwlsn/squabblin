976e9b5db8c5ea8b02a25af90996dc8a07d409e4
Danny Wilson <dannywilson32@gmail.com>
2014-02-09 07:04:53 +0000

    form validation on questions

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -1,5 +1,7 @@
 class Argument < ActiveRecord::Base
 
+	validates :text, presence: true, length: { maximum: 300 }
+
 	belongs_to :question
 	has_many :comments
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ActiveRecord::Base
 
 	validates :title, presence: true
+	validates :args_a, presence: true
+	validates :args_b, presence: true
 
 	has_many :arguments
 
